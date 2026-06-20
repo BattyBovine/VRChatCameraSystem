5df0f2a92b738ac1d8ad2927313ba7f346676c12
Jamie Greunbaum <BattyBovine+github@battybovine.com>
2026-06-20 12:17:16 -0400

    Removed lots of extraneous code we don't need.

--- Ottpossum/Camera System/Scripts/CameraSystem_Console.cs
+++ Ottpossum/Camera System/Scripts/CameraSystem_Console.cs
@@ -4,7 +4,6 @@ using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 using VRC.SDK3.Components;
-using VRC.SDK3.Data;
 using VRC.SDKBase;
 using VRC.Udon.Common;
 
@@ -37,9 +36,6 @@ namespace CameraSystem {
 		private int lastCamera = 5;
 		[HideInInspector] [UdonSynced] public string[] cameraFollowUsername = new string[6];
 		[HideInInspector] [UdonSynced] public bool[] cameraFollow = new bool[6];
-		[HideInInspector] [UdonSynced] private string _jsonPlayersList = "";
-		private DataList _playersList = new DataList();
-		//[UdonSynced] public float[] cameraFOV = new float[6];
 
 		[SerializeField] private ViewTabletSpawner _ViewTabletSpawner;
 
@@ -376,216 +372,19 @@ namespace CameraSystem {
 				Debug.Log($"[OTT_CAMERA_SYSTEM][sendLiveCamera6] Unauthorized action.");
 			}
 		}
-
-		public override void OnPlayerJoined(VRCPlayerApi player) {
-			if (!Utilities.IsValid(player)) { return; }
-			addPlayer(player);
-		}
-
-		public override void OnPlayerLeft(VRCPlayerApi player) {
-			if (!Utilities.IsValid(player)) { return; }
-			// remove player from any listing if they leave the world
-			removePlayer(player);
-		}
 		
-		public override void OnPreSerialization() {
-			if (lastCamera != currentCamera) {
-				Debug.Log($"[OTT_CAMERA_SYSTEM][OnDeserialization] Last Camera: {lastCamera}, New Camera: {currentCamera}");
-				SendLiveCamera(currentCamera);
-			}
-
-			// Convert the list _playersList to the network-synced variable _jsonPlayersList
-			if (VRCJson.TrySerializeToJson(_playersList, JsonExportType.Minify, out DataToken result)) {
-				Debug.Log($"[OTT_CAMERA_SYSTEM] OnPreSerialization: {_playersList.ToArray()} -> {result.String}");
-				_jsonPlayersList = result.String;
-			} else {
-				Debug.LogError($"[OTT_CAMERA_SYSTEM] OnPreSerialization error: {result.ToString()}");
-			}
-			// No update because we are PreSerializing from RequestSerialization and updatePlayerlist is called after it
-		}
 
 		public override void OnDeserialization() {
-			// Convert the network-synced variable _jsonPlayersList and convert it to a list in _playersList
-			if(VRCJson.TryDeserializeFromJson(_jsonPlayersList, out DataToken result)) {
-				Debug.Log($"[OTT_CAMERA_SYSTEM] OnDeserialization: {_jsonPlayersList} -> {result.DataList.ToArray()}");
-				_playersList = result.DataList;
-			} else {
-				Debug.LogError($"[OTT_CAMERA_SYSTEM] OnDeserialization error: {result.ToString()}");
-			}
-			// Then update the players list
-			updatePlayerlists();
 			SendLiveCamera(currentCamera);
 		}
 
-		public void addPlayer(VRCPlayerApi player)
+		public override void OnOwnershipTransferred(VRCPlayerApi Player)
 		{
-			// Invalid or already contains the user ? skip
-			if (!Utilities.IsValid(player)) { return; }
-			if (_playersList.Contains(player.displayName)) { return; }
-
-			_playersList.Add((string)player.displayName);
-			Debug.Log($"[OTT_CAMERA_SYSTEM] Player '{player.displayName}' added in the list");
-
-			// Only set ownership right before RequestSerialization and after checking if we really need to add the user
-			// to avoid spamming ownership transfert (which makes nothing work properly)
-			Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-			RequestSerialization();
-			// Workaround for https://github.com/vrchat-community/ClientSim/issues/74
-#if UNITY_EDITOR
-			Debug.Log($"[OTT_CAMERA_SYSTEM] Running UNITY_EDITOR only workaround");
-			OnPreSerialization();
-#endif
-			updatePlayerlists();
-		}
-
-		public void removePlayer(VRCPlayerApi player) {
-			// Invalid or not in the list ? skip
-			if (!Utilities.IsValid(player)) { return; }
-			if (!_playersList.Contains(player.displayName)) { return; }
-
-			_playersList.Remove((string)player.displayName);
-			Debug.Log($"[OTT_CAMERA_SYSTEM] Player '{player.displayName}' removed from the list");
-
-			// Only set ownership right before RequestSerialization and after checking if we really need to remove the user
-			// to avoid spamming ownership transfert (which makes nothing work properly)
-			Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-			RequestSerialization();
-			// Workaround for https://github.com/vrchat-community/ClientSim/issues/74
-#if UNITY_EDITOR
-			Debug.Log($"[OTT_CAMERA_SYSTEM] Running UNITY_EDITOR only workaround");
-			OnPreSerialization();
-#endif
-			updatePlayerlists();
-		}
-
-		public void _setSelectedUsername(string player, int cameraId) {
-			if (!isAuthorized) {
-				Debug.Log($"[OTT_CAMERA_SYSTEM][_setSelectedUsername] Unauthorized action.");
-				return;
+			foreach (Camera CameraObject in camerasObjects)
+			{
+				Networking.SetOwner(Player, CameraObject.gameObject);
 			}
-			Debug.Log($"[OTT_CAMERA_SYSTEM][_setSelectedUsername] Setting follow user {player} for camera {cameraId}");
-			switch (cameraId) {
-				case 1:
-					cameraFollowUsername[0] = player;
-					break;
-				case 2:
-					cameraFollowUsername[1] = player;
-					break;
-				case 3:
-					cameraFollowUsername[2] = player;
-					break;
-				case 4:
-					cameraFollowUsername[3] = player;
-					break;
-				case 5:
-					cameraFollowUsername[4] = player;
-					break;
-				case 6:
-					cameraFollowUsername[5] = player;
-					break;
-			}
-			Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-			RequestSerialization();
+			base.OnOwnershipTransferred(Player);
 		}
-
-		void updatePlayerlists() {
-			// TODO just reset the "current username" field with the first user in the list
-		}
-
-		private void updateCameraFovs() {
-			//Debug.Log($"[OTT_CAMERA_SYSTEM][updateCameraFovs]");
-			//camerasObjects[0].fieldOfView = cameraFOV[0];
-			//camerasObjects[1].fieldOfView = cameraFOV[1];
-			//camerasObjects[2].fieldOfView = cameraFOV[2];
-			//camerasObjects[3].fieldOfView = cameraFOV[3];
-			//camerasObjects[4].fieldOfView = cameraFOV[4];
-			//camerasObjects[5].fieldOfView = cameraFOV[5];
-			
-			//deskCamerasFovTexts[0].text = $"FOV {cameraFOV[0]}";
-			//deskCamerasFovTexts[1].text = $"FOV {cameraFOV[1]}";
-			//deskCamerasFovTexts[2].text = $"FOV {cameraFOV[2]}";
-			//deskCamerasFovTexts[3].text = $"FOV {cameraFOV[3]}";
-			//deskCamerasFovTexts[4].text = $"FOV {cameraFOV[4]}";
-			//deskCamerasFovTexts[5].text = $"FOV {cameraFOV[5]}";
-			
-			//handheldsFovTexts[0].text = $"FOV {cameraFOV[0]}";
-			//handheldsFovTexts[1].text = $"FOV {cameraFOV[1]}";
-			//handheldsFovTexts[2].text = $"FOV {cameraFOV[2]}";
-			//handheldsFovTexts[3].text = $"FOV {cameraFOV[3]}";
-			//handheldsFovTexts[4].text = $"FOV {cameraFOV[4]}";
-			//handheldsFovTexts[5].text = $"FOV {cameraFOV[5]}";
-		}
-
-		//public void _camera1FovChanged() {
-		//	if (!isAuthorized) {
-		//		Debug.Log($"[OTT_CAMERA_SYSTEM][_camera1FovChanged] Unauthorized action.");
-		//		cameraFovSliders[0].value = cameraFOV[0];
-		//		return;
-		//	}
-		//	cameraFOV[0] = cameraFovSliders[0].value;
-		//	Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-		//	RequestSerialization();
-		//	updateCameraFovs();
-		//}
-
-		//public void _camera2FovChanged() {
-		//	if (!isAuthorized) {
-		//		Debug.Log($"[OTT_CAMERA_SYSTEM][_camera2FovChanged] Unauthorized action.");
-		//		cameraFovSliders[1].value = cameraFOV[1];
-		//		return;
-		//	}
-		//	cameraFOV[1] = cameraFovSliders[1].value;
-		//	Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-		//	RequestSerialization();
-		//	updateCameraFovs();
-		//}
-
-		//public void _camera3FovChanged() {
-		//	if (!isAuthorized) {
-		//		Debug.Log($"[OTT_CAMERA_SYSTEM][_camera3FovChanged] Unauthorized action.");
-		//		cameraFovSliders[2].value = cameraFOV[2];
-		//		return;
-		//	}
-		//	cameraFOV[2] = cameraFovSliders[2].value;
-		//	Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-		//	RequestSerialization();
-		//	updateCameraFovs();
-		//}
-
-		//public void _camera4FovChanged() {
-		//	if (!isAuthorized) {
-		//		Debug.Log($"[OTT_CAMERA_SYSTEM][_camera4FovChanged] Unauthorized action.");
-		//		cameraFovSliders[3].value = cameraFOV[3];
-		//		return;
-		//	}
-		//	cameraFOV[3] = cameraFovSliders[3].value;
-		//	Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-		//	RequestSerialization();
-		//	updateCameraFovs();
-		//}
-
-		//public void _camera5FovChanged() {
-		//	if (!isAuthorized) {
-		//		Debug.Log($"[OTT_CAMERA_SYSTEM][_camera5FovChanged] Unauthorized action.");
-		//		cameraFovSliders[4].value = cameraFOV[4];
-		//		return;
-		//	}
-		//	cameraFOV[4] = cameraFovSliders[4].value;
-		//	Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-		//	RequestSerialization();
-		//	updateCameraFovs();
-		//}
-
-		//public void _camera6FovChanged() {
-		//	if (!isAuthorized) {
-		//		Debug.Log($"[OTT_CAMERA_SYSTEM][_camera6FovChanged] Unauthorized action.");
-		//		cameraFovSliders[5].value = cameraFOV[5];
-		//		return;
-		//	}
-		//	cameraFOV[5] = cameraFovSliders[5].value;
-		//	Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-		//	RequestSerialization();
-		//	updateCameraFovs();
-		//}
 	}
 }
